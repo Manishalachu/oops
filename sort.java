9eabbb0a80c1a72ca49c4e38a1a07536400f9fef
Manishalachu <manishalachu2005@gmail.com>
2026-01-29 11:21:57 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class sort
+{
+public static void main (String args[])
+{
+int n,i,j;
+String temp;
+Scanner sc=new Scanner(System.in);
+Scanner st=new Scanner(System.in);
+System.out.println("enter number of strings");
+n=sc.nextInt();
+String[] str=new String[n];
+System.out.println("enter the strings:");
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++
+ 
